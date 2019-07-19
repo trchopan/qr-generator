9cba23a52bf6477ddc1827ad48db19b862bd458a
Quang Tran <quang.tran@chatq.sg>
2019-07-19 15:24:45 +0700

    :hammer: Update build script to work with css files

---
+++ remove-hash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp dist/js/app.*.js dist/js/app.js
+cp dist/js/chunk-vendors.*.js dist/js/chunk-vendors.js
+cp dist/css/app.*.css dist/css/app.css
+cp dist/css/chunk-vendors.*.css dist/css/chunk-vendors.css
